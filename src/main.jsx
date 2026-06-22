c885a869d52f465775e33e9c8934cb7f87b8f2cd
KimonoGod <affeverruckter@gmail.com>
2026-06-22 21:33:20 +0200

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Chino from './Chino';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Chino />
-  </React.StrictMode>
-);
