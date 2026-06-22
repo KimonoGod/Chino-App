7ca7a67138158fa05fa75f49ed5ea2ab1b48013a
KimonoGod <affeverruckter@gmail.com>
2026-06-22 21:24:35 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Chino from './Chino';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Chino />
+  </React.StrictMode>
+);
