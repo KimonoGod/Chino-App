f17f501e9a7f6094575736d4e4f70b605194f2c5
KimonoGod <affeverruckter@gmail.com>
2026-06-22 21:20:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,1 @@
-mport React from 'react'
-import ReactDOM from 'react-dom/client'
-import Chino from './Chino'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Chino />
-  </React.StrictMode>
-)
